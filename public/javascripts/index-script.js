c8728a1e3f68221fc390ff5705b4845497632178
Elaine Zhou <ezhou@stanford.edu>
2015-03-08 14:02:57 -0700

    clean up some tile styling

---
+++ public/javascripts/index-script.js
@@ -0,0 +1,37 @@
+function aboutMargin(event){
+
+  var bHeight = $('.blurb').height();
+  var aHeight = $('.about').height(); // ??
+
+  if(bHeight > aHeight){
+    $('.blurb').css('padding-top', '0');
+    $('.about').css('padding-top', (bHeight - aHeight)/2 + 'px');
+  } else if (aHeight > bHeight){
+    $('.about').css('padding-top', '0');    
+    $('.blurb').css('padding-top', (aHeight - bHeight)/2 + 'px');
+  }
+
+  var maxHeight;
+  if($('.about').css('display') != 'none'){
+    maxHeight = Math.max(aHeight, bHeight);
+  } else{
+    maxHeight = bHeight;
+  }
+
+  var topMargin = ($(window).height() - maxHeight) / 2;
+  $('#about > .row').css('padding-top', topMargin + 'px');
+}
+
+$(document).on('ready', function(){
+
+  $('.arrow').find('a').click(scrollToSection);
+
+  // do fading 5 times
+  for(i=0;i<5;i++) {
+    $(".arrow").fadeTo('slow', 0.2).fadeTo('slow', 1.0);
+  }
+
+  aboutMargin();
+  $(window).on('resize', aboutMargin);
+
+});
